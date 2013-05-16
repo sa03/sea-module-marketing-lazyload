ab6b26c8da760d95a0eedb08b593fe09d3369e7f
Pu Shiming <fahai@taobao.com>
2013-05-16 15:08:56 +0800

    A big deal

---
+++ Gruntfile.js
@@ -0,0 +1,77 @@
+/*global module:false*/
+module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+
+        // Metadata.
+        pkg: grunt.file.readJSON('package.json'),
+        banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+            '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+            '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+            '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+            ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+        cmdWrapper: {
+            banner: 'define(function(require, exports, module) { \n var Zepto = require("<%= pkg.spm.alias.zepto %>"); \n',
+            footer: '\n }); \n'
+        },
+
+        // Task configuration.
+        concat: {
+            cmdWrap: {
+                options: {
+                    noncmd: true,
+                    banner: '<%= cmdWrapper.banner %>',
+                    footer: '<%= cmdWrapper.footer %>'
+                },
+                files: [
+                    {
+                        src: ['lib/<%= pkg.name %>.js'],
+                        dest: 'tmp/<%= pkg.name %>.js'
+                    }
+                ]
+            }
+        },
+
+        transport: {
+            options: {
+                idleading: '<%= pkg.family %>/<%= pkg.name %>/<%= pkg.version %>/',
+                debug: true
+            },
+            dist: {
+                files: [
+                    {
+                        cwd: 'tmp',
+                        src: '**/*',
+                        filter: 'isFile',
+                        dest: 'dist'
+                    }
+                ]
+            }
+        },
+
+        uglify: {
+            options: {
+                banner: '<%= banner %>'
+            },
+            dist: {
+                src: 'dist/<%= pkg.name %>.js',
+                dest: 'dist/<%= pkg.name %>.js'
+            }
+        },
+        clean: {
+            temp: ['tmp'],
+            dist: ['dist']
+        }
+    });
+
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-cmd-concat');
+    grunt.loadNpmTasks('grunt-cmd-transport');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+
+    // Default task.
+    grunt.registerTask('default', ['clean:dist', 'concat', 'transport', 'uglify', 'clean:temp']);
+
+};
