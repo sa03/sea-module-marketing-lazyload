54e705693f78c645755478a18baf35b175cfff76
Pu Shiming <fahai@taobao.com>
2013-07-22 19:02:55 +0800

    Demo improved...

--- demo/main.js
+++ demo/main.js
@@ -1,5 +1,5 @@
 define(function (require, exports, module) {
-    var $ = require('marketing/zepto/1.0.0/zepto');
+    var $ = require('zepto/zepto/1.0.0/zepto');
     require('marketing/lazyload/1.8.4/lazyload');
 
     $("img").lazyload();
